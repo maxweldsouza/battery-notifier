a58027f37b48e3bb25ec3dea21ec11cac80e4969
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2024-03-30 13:47:03 +0530

    fix: Status display

--- src/renderer/Battery.tsx
+++ src/renderer/Battery.tsx
@@ -9,9 +9,12 @@ import Tr from '../shared/Tr';
 import Th from '../shared/Th';
 import Thead from '../shared/Thead';
 import Tbody from '../shared/Tbody';
+import Status from './Status';
 
 const MIN_IN_MILLISECONDS = 60 * 1000;
 const REFRESH_INTERVAL = 1 * MIN_IN_MILLISECONDS;
+
+const humanizeStatus = status => status?.replace('-', ' ')
 
 function Battery(props) {
   const [data, setData] = useState([]);
@@ -79,15 +82,16 @@ function Battery(props) {
               {row.percentage}%
             </Th>
             <Th>
-              {/*0: Unknown*/}
-              {/*1: Charging*/}
-              {/*2: Discharging*/}
-              {/*3: Empty*/}
-              {/*4: Fully charged*/}
-              {/*5: Pending charge*/}
-              {/*6: Pending discharge*/}
-
-              {row.state}
+              <Status>
+                {/*0: Unknown*/}
+                {/*1: Charging*/}
+                {/*2: Discharging*/}
+                {/*3: Empty*/}
+                {/*4: Fully charged*/}
+                {/*5: Pending charge*/}
+                {/*6: Pending discharge*/}
+                {humanizeStatus(row.state)}
+              </Status>
             </Th>
             <Th>
 
---
+++ src/renderer/Status.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Status = styled.div`
+  text-transform: capitalize;
+`
+
+export default Status;
