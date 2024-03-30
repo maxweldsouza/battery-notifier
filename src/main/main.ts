8d7ae2fda4ce1f3682f405ae78eb5d4d8cb25b8a
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2024-03-30 16:35:45 +0530

    fix: Remove example code

--- src/main/main.ts
+++ src/main/main.ts
@@ -50,7 +50,6 @@ ipcMain.on('electron-store-get', async (event, val) => {
   event.returnValue = store.get(val);
 });
 ipcMain.on('electron-store-set', async (event, key, val) => {
-  console.log(val)
   if (val) {
     store.set(key, val);
   }
