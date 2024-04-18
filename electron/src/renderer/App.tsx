01773a88b6aef98029e5de6e576460f2c5924add
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2024-04-18 11:58:00 +0530

    feat: Add color scheme hook

--- electron/src/renderer/App.tsx
+++ electron/src/renderer/App.tsx
@@ -3,11 +3,13 @@ import '../shared/globals.css';
 import { ThemeProvider } from 'styled-components';
 import Battery from './Battery';
 import { getTheme } from '../shared/theme';
+import useColorScheme from '../shared/hooks/useColorScheme';
 
 export default function App() {
+  const colorScheme = useColorScheme();
   return (
     <Router>
-      <ThemeProvider theme={getTheme('dark')}>
+      <ThemeProvider theme={getTheme(colorScheme)}>
         <Routes>
           <Route path="/" element={<Battery />} />
         </Routes>
