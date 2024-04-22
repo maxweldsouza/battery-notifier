b1d42759c5f05b196bd8dcacb7612edbf026983d
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2024-04-22 09:38:06 +0530

    feat: v0.2.0

--- install_snap.sh
+++ install_snap.sh
@@ -3,5 +3,5 @@ ts-node ./.erb/scripts/clean.js dist && npm run build
 cd ..
 snapcraft -v
 sudo snap remove battery-notifier
-sudo snap install battery-notifier_0.1.0_amd64.snap --dangerous
+sudo snap install battery-notifier_0.2.0_amd64.snap --dangerous
 snap run battery-notifier
