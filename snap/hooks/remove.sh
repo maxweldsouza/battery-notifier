2a6f09b8e077b3f1b7f30f1753ec42b4587448e0
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2024-05-13 15:03:06 +0530

    fix: Remove autorun

--- snap/hooks/remove.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Destination path
-destinationPath="$SNAP_USER_DATA/.config/autostart/battery-notifier.desktop"
-
-# Check if the file exists and remove it
-if [ -f "$destinationPath" ]; then
-  rm "$destinationPath"
-fi
